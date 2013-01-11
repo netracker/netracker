5fe97b913da8842c799e6ac6ae5c2f5956adbe3d
drew <drew@drewolson.org>
2013-01-11 16:22:16 -0600

    Format server.

--- src/netracker/server/server.go
+++ src/netracker/server/server.go
@@ -1,49 +1,48 @@
 package server
 
 import (
-        "netracker/connection_manager"
-        "code.google.com/p/go.net/websocket"
-        "net/http"
-        "log"
+	"code.google.com/p/go.net/websocket"
+	"log"
+	"net/http"
+	"netracker/connection_manager"
 )
 
 var globalManager = connection_manager.New()
 
 func receiveWebsocketMessage(conn *websocket.Conn) (message string) {
-        err := websocket.Message.Receive(conn, &message)
-        if err != nil {
-                log.Printf("Got error sending WS message: %v", err)
-                return
-        }
+	err := websocket.Message.Receive(conn, &message)
+	if err != nil {
+		log.Printf("Got error sending WS message: %v", err)
+		return
+	}
 
-        return
+	return
 }
 
 func reader(conn *websocket.Conn) {
-        for {
-                message := receiveWebsocketMessage(conn)
-								log.Printf("Got message: %v", message)
+	for {
+		message := receiveWebsocketMessage(conn)
+		log.Printf("Got message: %v", message)
 
-        }
+	}
 
-        conn.Close()
+	conn.Close()
 }
 
 func wsHandler(conn *websocket.Conn) {
-        globalManager.AddConn(conn)
-        reader(conn)
+	globalManager.AddConn(conn)
+	reader(conn)
 }
 
-
 func Run() {
-        log.Print("Starting Netracker Server: http://localhost:3000")
-        go globalManager.Run()
+	log.Print("Starting Netracker Server: http://localhost:3000")
+	go globalManager.Run()
 
-        http.Handle("/", http.FileServer(http.Dir("public")))
-        http.Handle("/ws", websocket.Handler(wsHandler))
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.Handle("/ws", websocket.Handler(wsHandler))
 
-        err := http.ListenAndServe(":3000", nil)
-        if err != nil {
-                log.Printf("error %v", err)
-        }
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Printf("error %v", err)
+	}
 }
